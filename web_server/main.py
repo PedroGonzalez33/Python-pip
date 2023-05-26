7c2026774d73d873b50950f18bd6f32d18f78db0
Peter33 <pedro3gonzalez3@gmail.com>
2023-05-26 03:06:23 -0400

    Fastapi and Unicorn

--- web_server/main.py
+++ web_server/main.py
@@ -1,7 +1,22 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI()
+
+@app.get('/', response_class=HTMLResponse)
+def get_list():
+    return '''
+    <h1> Hola soy peter33 </h1>
+    <p> Hi i'm peter33 </p>
+    '''
+
+@app.get('/contact')
+def get_list():
+    return {'name':'peter33'}
+    
 def run():
-    store.get_products()
+    store.get_product
 
 if __name__ == '__main__':
     run()
